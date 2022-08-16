363ee5a7249bdb657c07199cd0d8fc675f112101
steve <stevemighty2030@gmail.com>
2022-08-16 13:51:36 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+
+function shout(string) {
+    return string.toUpperCase();
+
+  }
+function whisper(string) {
+    return string.toLowerCase();
+
+
+  }
+function  logShout(string){
+
+    console.log(string.toUpperCase());
+}
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string){
+    if (string.toLowerCase()) {
+       console.log("I can't hear you!" )
+      }
+
+}
+function sayHiToHeadphonedRoommate(string){
+    let String =HELLO;
+    if (string.toLowerCase()) {
+       console.log("YES INDEED!" )
+      }
+
+}
+function sayHiToHeadphonedRoommate(string){
+    let String=Hello;
+    if (string.toLowerCase()) {
+       console.log("I love you, Grandma." )
+      }
+
+}
+
